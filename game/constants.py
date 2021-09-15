6c85ccac817d28ec7863c3aa110001f8695ac611
Dominique <niehau@gmail.com>
2021-09-15 11:12:21 +0200

    updated gitignore and created class for checkers pieces

--- game/constants.py
+++ game/constants.py
@@ -5,9 +5,11 @@ WIDTH, HEIGHT = 800, 800
 ROWS, COLS = 8, 8
 SQUARE_SIZE =  WIDTH//COLS
 
-#colors
-RED = (255, 0, 0)
+#colors red=lightbrown black=darkbrown
+RED = (188, 158, 130)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+BLACK = (101, 67, 33)
+ACTUAL_BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
